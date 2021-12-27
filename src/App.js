539243d157f7aed507ff115b290cef7f8abebe42
Zinan Guo <znaaron@ZinandeAir.local>
2021-12-26 20:03:27 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Router } from "@reach/router";
+
+import Posts from './components/posts'
+import Post from './components/post'
+
+function App() {
+  return (
+    Posts()
+  );
+}
+
+export default App;
